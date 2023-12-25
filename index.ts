983357a2632bd758153d84a924f0b4d64ed4d537
olestatchyn <oles2709@gmail.com>
2023-12-25 18:41:26 +0200

    Adding simplified version of the top CLI utility program

--- index.ts
+++ index.ts
@@ -1,4 +1,40 @@
-function getRandomNumber(): number {
-    return Math.random();
-}
-console.log(getRandomNumber());
+// const oranges = ['orange', 'orange'];
+// const apples = ['just one apple'];
+
+// oranges.forEach(fruit => {
+//  console.count(fruit);
+// });
+
+// apples.forEach(fruit => {
+//  console.count(fruit);
+// });
+
+
+// const function2 = () => console.trace();
+// const function1 = () => function2();
+
+// function1();
+
+
+// const doSomething = () => console.log('test');
+
+// const measureDoingSomething = () => {
+//  console.time('doSomething()');
+//  // do something, and measure the time it takess
+//  doSomething();
+//  console.timeEnd('doSomething()');
+// };
+
+// measureDoingSomething();
+
+// const authorsAndBooks = [
+//     ['Tolkien', 'Lord of The Rings'],
+//     ['Rutger', 'Utopia For Realists'],
+//     ['Sinek', 'Leaders Eat Last'],
+//     ['Eyal', 'Habit'],
+//    ];
+   
+//    console.table(authorsAndBooks);
+//    const inventory = { apples: 200, mangoes: 50, avocados: 300, kiwis: 50 };
+   
+//    console.table(inventory);
